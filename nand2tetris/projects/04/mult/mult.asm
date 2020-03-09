af46b87b0db6f0968c9565dd7ba9d5be967c54d0
margaretcruse <maggiecruse@gmail.com>
2020-03-09 10:33:14 -0500

    mult done

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R2
+M=0
+
+(LOOP)
+@R2
+D=M
+@R0
+D=D+M
+
+@R2
+M=D
+
+@R1
+D=M-1
+M=D
+
+@LOOP
+D;JGT
+
+(END)
+@END
+0;JMP
